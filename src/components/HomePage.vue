0fb2f88fc365d371ff1ac8f50eabbe739ed75476
DrAtsiSama <dareka@outlook.fr>
2024-01-11 23:03:53 +0100

    voicemod

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,3 +1,11 @@
+<style>
+    .active{
+        background-color: green;
+    }
+    .inactive{
+        background-color: red;
+    }
+</style>
 <template>
     <div class="w-full h-full">
         <nav class="h-16">
@@ -23,9 +31,17 @@
                     </div>
                 </div>
                 <div class="flex">
+                    
                     <input :disabled="isLoading" type="text" placeholder="Type here"
-                                    class="input input-bordered input-secondary w-[416px] h-16 text-white" v-model="inputSearch"
+                                    class="input input-bordered input-secondary w-[416px]" id="transcript" v-model="inputSearch"
                                     @keyup.enter="handleSubmitInput" />
+                    <div class="speech">
+                        <button type="button" 
+                        :class="true ? 'active' : 'inactive'"
+                        class="input input-bordered input-secondary voiceInput"
+                        @click="startVoiceRecognition()">
+                    <img src="https://webocreation.com/wp-content/uploads/2022/02/voice-icons.png"/></button>
+                    </div>
                     <span v-show="isLoading" class="loading loading-spinner text-error loading-lg"></span>
                 </div>
             </div>
@@ -37,6 +53,7 @@
 import NavBar from './NavBar.vue'
 import bgImage from '../assets/images/scale.jpeg';
 import RecipesService from '../services/recipes.service';
+import * as annyang from "annyang";
 
 export default {
     name: 'HomePage',
@@ -67,6 +84,35 @@ export default {
                     console.log(error);
                 }
             )
+        },
+        startVoiceRecognition() {
+            if(annyang === null) return;
+            console.log("annyang : ", annyang.isListening());
+            console.log("web voice ? ", window.hasOwnProperty('webkitSpeechRecognition'));
+                if (!annyang.isListening()) {
+                    annyang.setLanguage('fr-FR'); // Définissez la langue selon vos besoins
+                    let commands = {
+                        'Hello' : recipes
+                    }
+                    function recipes(){
+                        var inputElement = document.getElementById('transcript');
+                        inputElement.value = "Recette de pates";
+                    }
+                    annyang.addCommands(commands);
+                    annyang.addCommands({
+                    'recherche *term': (term) => {
+                        // Traitez la saisie vocale ici (par exemple, effectuez une recherche et redirigez l'utilisateur)
+                        console.log('Recherche:', term);
+                        // Redirigez l'utilisateur vers la page de résultats de recherche
+                        this.$router.push({ path: '/search', query: { term } });
+                    },
+                    });
+                    annyang.start();
+                } else if (annyang.isListening()){
+                    annyang.abort();
+                } else {
+                    console.error("La reconnaissance vocale n'est pas prise en charge dans ce navigateur.");
+                }
         }
     },
 }
